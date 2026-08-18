a8b3a889a7bdaac6f9d4b70d74fa929943ef4d5c
Joshua Basubas <joshuastan_basubas@aura.edu.ph>
2026-08-18 15:32:34 +0800

    test

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/matta">
+    <BrowserRouter basename="/matta-portfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
